fa18a57d58cb261ec13c72bade26aba24bca384d
Zero-Bug401 <83648700+Zero-Bug401@users.noreply.github.com>
2021-05-04 14:13:59 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/app'; 
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
